791a97545206112929f0cb21be127d39661dd1f5
Haniel Cedraz <haniel@Haniels-MacBook-Air.local>
2022-04-02 12:51:57 -0300

    Bowtie genome buiilding

--- miRbaqcomMapping-Bowtie.R
+++ miRbaqcomMapping-Bowtie.R
@@ -586,7 +586,9 @@ userInput <- function(question) {
 
 if (opt$mappingProgram == "bowtie") {
     
-    if (!all(file.exists(list_files_with_exts(index_Folder, exts = "ebwt")))) {
+    if (all(file.exists(list_files_with_exts(index_Folder, exts = "ebwt")))) {
+        write(glue("\n\n {str_dup('-', 100)} \n\n {str_dup(' ', 40)} Genome is built and all is set for mapping \n\n {str_dup('-', 100)} \n\n"), stdout())
+    } else {
         write(glue("\n\n {str_dup('-', 100)} \n\n {str_dup(' ', 40)} Buiding genome started \n\n {str_dup('-', 100)} \n\n"), stdout())
         
         index_genom <- indexBuiding(program = opt$mappingProgram, opt$mappingTarget, index_Folder)
